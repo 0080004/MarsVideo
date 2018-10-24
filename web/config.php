13e7a6641c2d48de550b5c56b5571691e312fdaf
0080004 <32374971+0080004@users.noreply.github.com>
2018-10-24 10:58:56 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
+define('APIKEY', 'AIzaSyBrN0UZw8917HCOfQbNPMeHSHZl3fq3XQg');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
+define('SITE_NAME', '来自火星的视频');//网站名字
+define('TITLENAME', '火星视频');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', 'michlsfejxga@yahoo.com');//邮箱
 ?>
